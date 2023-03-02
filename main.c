1ec6f846978ecc53751c49f3df578201e2d5c707
jyand <jyand@protonmail.com>
2023-03-02 14:53:49 -0500

    test over bus reader

--- main.c
+++ main.c
@@ -6,14 +6,26 @@
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 #include <util/delay.h>
+#include "acican.h"
+#include "lxcan.h"
+
+#define DELAY 100
 
 void main() __attribute__((noreturn)) ;
 
 void main() {
         asm("cli ; wdr ;") ;
         InitCAN() ;
+        struct CANPacket *packet ;
+        packet = (struct CANPacket*)malloc(sizeof(struct CANPacket)) ;
+        packet->devclass = 0x20 ;
+        packet->devID = 0x00 ;
+        packet->subID = 0x81 ;
+        packet->data[] = 0x15B01 ;
         for (;;) {
                 asm("wdr ;") ;
+                SendCANPacket(packet) ;
+                _delay_ms(DELAY) ;
                 sleep_disable() ;
         }
 }
