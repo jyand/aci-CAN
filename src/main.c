56e87dca6631657c091514c11c115ebb1383b303
jyand <jyand@protonmail.com>
2023-03-14 11:01:25 -0400

    can't believe this works

--- src/main.c
+++ src/main.c
@@ -14,10 +14,10 @@ void main() {
         packet->data[0] = 0x1 ;
         packet->data[1] = 0x5B ;
         packet->data[2] = 0x1 ;
-        packet->data[4] = 0xDE ;
-        packet->data[5] = 0xAD ;
-        packet->data[6] = 0xBE ;
-        packet->data[7] = 0xEF ;
+        packet->data[4] = 0x0D ;
+        packet->data[5] = 0xE5 ;
+        packet->data[6] = 0xA1 ;
+        packet->data[7] = 0x70 ;
         asm("sei ;") ;
         for (;;) {
                 asm("wdr ;") ;
